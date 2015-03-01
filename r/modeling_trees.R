2702c6bac771cddfa0ea8897854fb81c8a2a0429
Andee Kaplan <ajkaplan@iastate.edu>
2015-03-01 17:20:48 -0600

    random forest and features complete

--- r/modeling_trees.R
+++ r/modeling_trees.R
@@ -2,3 +2,15 @@
 #tree models
 #make sure to run validation and features first
 #depends on: preprocessing_validation.R, preprocessing_features.R
+
+## random forest -----------------------------------
+library(randomForest)
+rf <- randomForest(factor(label) ~ ., data = train_features)
+
+rf$importance %>% 
+  data.frame() %>%
+  mutate(variable = factor(rownames(.), levels = rownames(.)[order(MeanDecreaseGini)])) %>%
+  ggplot() +
+  geom_point(aes(MeanDecreaseGini, variable))
+
+## boosted trees ------------------------------------
