9b1dffca513a94f828d772ea2ea85c250d3a9f78
Andee Kaplan <ajkaplan@iastate.edu>
2015-03-02 19:05:01 -0600

    trees updates

--- r/modeling_trees.R
+++ r/modeling_trees.R
@@ -3,7 +3,7 @@
 #make sure to run validation and features first
 #depends on: preprocessing_validation.R, preprocessing_features.R
 
-## random forest -----------------------------------
+## random forest ------------------------------------------
 library(randomForest)
 rf <- randomForest(factor(label) ~ ., 
                    data = train_features %>% select(-id),
@@ -20,17 +20,79 @@ rf.predict <- predict(rf, validate_features, type = "class")
 #8.7% validation error 
 #mean(as.numeric(as.character(rf.predict)) != validate_features$label)
 
-## boosted trees ------------------------------------
+## boosted trees ------------------------------------------------
 library(gbm)
-expand.grid(shrinkage = .1, depth = 1:10) %>%
-  group_by(shrinkage, depth) %>%
-  do(model = gbm(formula = factor(label) ~ .,
-                 data = train_features %>% select(-id),
-                 distribution = "multinomial",
-                 shrinkage = .$shrinkage,
-                 interaction.depth = .$depth,
-                 n.trees = 500)) -> boosted.trees
+
+boosted.results <- function(shrinkage, depth) {
+  expand.grid(shrinkage = shrinkage, depth = depth) %>%
+    group_by(shrinkage, depth) %>%
+    do(model = gbm(formula = factor(label) ~ .,
+                   data = train_features %>% select(-id),
+                   distribution = "multinomial",
+                   shrinkage = .$shrinkage,
+                   interaction.depth = .$depth,
+                   n.trees = 500)) -> boosted.trees
+  
+  for(i in 1:nrow(boosted.trees)){
+    boosted.trees[i, "train_error"] <- boosted.trees[i,]$model[[1]]$train.error[boosted.trees[i,]$model[[1]]$n.trees]
+  }
+  boosted.trees %>%
+    group_by(shrinkage, depth, train_error) %>%
+    do(pred = predict(.$model[[1]], validate_features %>% select(-id, -label), n.trees = .$model[[1]]$n.trees, type = "response")) -> gbm.predict
+  
+  gbm.predict %>%
+    group_by(shrinkage, depth, train_error) %>%
+    do(prediction = apply(.$pred[[1]][,,1],1, which.max)) %>%
+    group_by(shrinkage, depth, train_error) %>%
+    mutate(test_error = mean(as.character(prediction[[1]] - 1) != validate_features$label)) -> gbm.results
+  
+  return(list(model = boosted.trees, preds = gbm.predict, res = gbm.res))  
+}
+
+boosted_1_5 <- boosted.results(c(.1, .2), 1:5)
+boosted_6_10 <- boosted.results(c(.1, .2), 6:10)
+boosted_11_15 <- boosted.results(c(.1, .2), 11:15)
+
+## PPtree -------------------------------------------
+library(PPtree)
+#must remove variables with no variation
+pptree <- LDA.Tree(train_features$label,  
+                   train_features %>% 
+                     ungroup() %>% 
+                     select(-id, -label, -col_1, -row_1, -row_2, -col_2, -col_27, -col_28))
+
+pptree.predict <- PP.classify(validate_features %>% 
+                              ungroup() %>% 
+                              select(-id, -label, -col_1, -row_1, -row_2, -col_2, -col_27, -col_28),
+                            validate_features$label,
+                            pptree)
+
+#31% validation error 
+#mean(as.character(pptree.predict$predict.class) != validate_features$label)
 
 
 ## save models because they take a long time to run ----------------------------
 save(rf, boosted.trees, file = "written_results/models.RData")
+save(rf.predict, gbm.predict, file = "written_results/predict.RData")
+
+## submodels 3, 5, 9, 4 ----------------------------
+rf_35 <- randomForest(factor(label) ~ ., 
+                   data = train_features %>% select(-id) %>% filter(label %in% c("3", "5")),
+                   ntree = 1000)
+
+rf_49 <- randomForest(factor(label) ~ ., 
+                      data = train_features %>% select(-id) %>% filter(label %in% c("4", "9")),
+                      ntree = 1000)
+
+
+## chain random forests
+rf.pred_35 <- predict(rf_235, validate_features[which(as.character(rf.predict) %in% c("3", "5")),])
+rf.pred_49 <- predict(rf_49, validate_features[which(as.character(rf.predict) %in% c("4", "9")),])
+
+rf.pred_combo <- rf.predict
+rf.pred_combo[which(as.character(rf.predict) %in% c("3", "5"))] <- rf.pred_35
+rf.pred_combo[which(as.character(rf.predict) %in% c("4", "9"))] <- rf.pred_49
+
+#8.3% validation error 
+#mean(as.character(rf.pred_combo) != validate_features$label)
+
